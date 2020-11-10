b4d1cdd0e4a47b5c4aa2f18915f2af61b63db801
Frank Silva <frankjoesilva@gmail.com>
2020-11-09 22:42:03 -0800

    changed made to login

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken')
-const{ JWT_SECRET } = process.env
+const { JWT_SECRET } = process.env
 const usersRouter = express.Router();
 const { requireUser } = require('./utils')
 const { createUser, getUserByUsername, getUser } = require('../db/users');
@@ -9,23 +9,24 @@ const { getPublicRoutinesByUser } = require('../db/routines')
 
 
 usersRouter.post('/register', async (req, res, next) => {
-    const { username, password } = req.body;
-    try {
-      const users = await getUserByUsername(username);
-        if (users) {
-            res.send({ message : 'A user by that username already exists'});
-        }
-      else if ( password.length <= 8 ) { 
-        res.send({message:'Password Too Short!'}); 
-      } else {
+  const { username, password } = req.body;
+  try {
+    const users = await getUserByUsername(username);
+    if (users) {
+      res.send({ message: 'A user by that username already exists' });
+    }
+    else if (password.length <= 8) {
+      res.send({ message: 'Password Too Short!' });
+    } else {
 
-        const user = await createUser({ username, password });
-        res.send({ message: "niceee", user : user});
-        return user
-      }
-    } catch (error) {
-      next(error)
-    } 
+      const user = await createUser({ username, password });
+      const token = jwt.sign(user, JWT_SECRET);
+      res.send({ message: "User Created", user: user, token });
+      return user
+    }
+  } catch (error) {
+    next(error)
+  }
 });
 
 usersRouter.post('/login', async (req, res, next) => {
@@ -37,29 +38,29 @@ usersRouter.post('/login', async (req, res, next) => {
       res.send({ message: "you're logged in!", token });
       return user;
     } else {
-      next({ name: 'IncorrectCredentialsError', message: 'Username or password is incorrect' });
+      return next({ name: 'IncorrectCredentialsError', message: 'Username or password is incorrect' });
     }
-  } catch(error) {
+  } catch (error) {
     next(error);
   }
 });
 
 usersRouter.get('/:username/routines', async (req, res, next) => {
   const { username } = req.params;
-  const getUsername = await getPublicRoutinesByUser({username})
-try {
+  const getUsername = await getPublicRoutinesByUser({ username })
+  try {
     res.send(getUsername)
-} catch ({ name, message }) {
-  next(error)
-}
+  } catch ({ name, message }) {
+    next(error)
+  }
 });
 
 usersRouter.get('/me', requireUser, async (req, res, next) => {
   try {
-      res.send(req.user);
+    res.send(req.user);
   }
-   catch ({ name, message }) {
-      next({ name, message });
+  catch ({ name, message }) {
+    next({ name, message });
   }
 });
 
--- db/users.js
+++ db/users.js
@@ -2,60 +2,63 @@ const client = require('./client');
 const bcrypt = require('bcrypt');
 const SALT_COUNT = 10
 
-async function createUser({ 
-  username, 
+async function createUser({
+  username,
   password
 }) {
   try {
     const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
-      const { rows: [ user ] } = await client.query(`
+    const { rows: [user] } = await client.query(`
       INSERT INTO users(username, password) 
       VALUES($1, $2) 
       ON CONFLICT (username) DO NOTHING 
       RETURNING *;
       `, [username, hashedPassword]);
-      delete user.password
-      return user;
-    } catch (error) {
-      throw error;
-    }
+    delete user.password
+    return user;
+  } catch (error) {
+    throw error;
   }
+}
 
-  async function getUserByUsername(username) {
-    try {
-      const { rows: [user] } = await client.query(`
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
         SELECT *
         FROM users
         WHERE username=$1
       `, [username]);
-  
-      return user;
-    } catch (error) {
-      throw error;
-    }
-  }
-  
 
- async function getUser({username, password}){
-   try{
-     const {rows:[user]} = await client.query(`
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+async function getUser({ username, password }) {
+  try {
+    const { rows: [user] } = await client.query(`
      SELECT *
      FROM users
      WHERE username=$1;
      `, [username])
-    const isAMatch = await bcrypt.compare(password, user.password); 
-    if(isAMatch){
+    const isAMatch = await bcrypt.compare(password, user.password);
+    if (isAMatch) {
       delete user.password
       return user
     }
-   }catch(error){
-     throw error
-   }
- }
+    else {
+      return false
+    }
+  } catch (error) {
+    throw error
+  }
+}
 
 async function getUserById(userId) {
   try {
-    const { rows: [ user ] } = await client.query(`
+    const { rows: [user] } = await client.query(`
       SELECT *
       FROM users
       WHERE id=$1
@@ -67,11 +70,11 @@ async function getUserById(userId) {
   }
 }
 
-  module.exports = {
-    client,
-    createUser,
-    getUserById,
-    getUser,
-    getUserByUsername,
-   
-  }
+module.exports = {
+  client,
+  createUser,
+  getUserById,
+  getUser,
+  getUserByUsername,
+
+}
